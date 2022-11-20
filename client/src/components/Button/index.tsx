51979c309c800e7e8a81e9a5cf0be18f6c73f9cf
Seungjae Lee <mathlife12345@gmail.com>
2022-11-20 20:52:43 +0900

    feat: 🎸 Button 공통 컴포넌트 제작
    - 버튼 컴포넌트를 제작하고 게임 시작, 방 만들기, 방 나가기, 수락, 거절, 확인, 닫기 스토리를 생성했다.
    - 차후 수정이 필요한 사항은 다음과 같다.
        - 버튼 사이즈를 임의로 지정했기 때문에 적절한 사이즈에 대한 합의가 필요하다.
        - 폰트를 지정해줄 필요가 있다.
        - 배경색을 hex color code로 정의했는데 글로벌 스타일 혹은 theme을 이용해 정의해 줄 필요가 있다.

---
+++ client/src/components/Button/index.tsx
@@ -0,0 +1,54 @@
+/** @jsxImportSource @emotion/react */
+
+import {
+  largeButtonStyle,
+  smallButtonStyle,
+  gameStartButtonStyle,
+  createRoomButtonStyle,
+  exitRoomButtonStyle,
+  acceptButtonStyle,
+  declineButtonStyle,
+  OKButtonStyle,
+  closeButtonStyle,
+} from "./styles";
+
+export interface ButtonProps {
+  buttonType: "방 만들기" | "방 나가기" | "게임 시작" | "확인" | "수락" | "닫기" | "거절";
+  handleClick: () => void;
+}
+
+function Button({ buttonType, handleClick }: ButtonProps) {
+  let style = null;
+
+  switch (buttonType) {
+    case "게임 시작":
+      style = [largeButtonStyle, gameStartButtonStyle];
+      break;
+    case "방 만들기":
+      style = [largeButtonStyle, createRoomButtonStyle];
+      break;
+    case "방 나가기":
+      style = [largeButtonStyle, exitRoomButtonStyle];
+      break;
+    case "수락":
+      style = [smallButtonStyle, acceptButtonStyle];
+      break;
+    case "거절":
+      style = [smallButtonStyle, declineButtonStyle];
+      break;
+    case "확인":
+      style = [smallButtonStyle, OKButtonStyle];
+      break;
+    case "닫기":
+      style = [smallButtonStyle, closeButtonStyle];
+      break;
+  }
+
+  return (
+    <button type="button" css={style} onClick={handleClick}>
+      {buttonType}
+    </button>
+  );
+}
+
+export default Button;
--- client/src/components/Logo/index.stories.tsx
+++ client/src/components/Logo/index.stories.tsx
@@ -2,23 +2,26 @@ import { Meta, Story } from "@storybook/react";
 import Logo, { LogoProps } from "./index";
 
 export default {
+  title: "공통 컴포넌트/로고",
   component: Logo,
-  title: "Logo",
 } as Meta;
 
 const Template: Story<LogoProps> = args => <Logo {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Both = Template.bind({});
+Both.storyName = "이미지 & 글자";
+Both.args = {
   logoType: "BOTH",
 };
 
 export const ImageOnly = Template.bind({});
+ImageOnly.storyName = "이미지";
 ImageOnly.args = {
   logoType: "IMAGE_ONLY",
 };
 
 export const WordOnly = Template.bind({});
+WordOnly.storyName = "글자";
 WordOnly.args = {
   logoType: "WORD_ONLY",
 };
