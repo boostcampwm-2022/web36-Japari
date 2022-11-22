def896b3075510528aa341c7e993ff89e005119d
kimhyeongjun95 <hjthelegend@naver.com>
2022-11-22 13:13:44 +0900

    feat: 🎸 input 컴포넌트 구현
    ✅ Closes: #9

---
+++ client/src/components/Input/input.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, Story } from "@storybook/react";
+import Input, { InputProps } from "./index";
+
+export default {
+  title: "공통 컴포넌트/인풋",
+  component: Input,
+} as Meta;
+
+const Template: Story<InputProps> = args => <Input {...args} />;
+
+export const Default = Template.bind({});
+Default.storyName = "인풋";
+Default.args = {
+  placeholder: "",
+};
---
+++ client/src/components/Input/styles.tsx
@@ -0,0 +1,18 @@
+import { css } from "@emotion/react";
+
+export const inputStyle = css`
+  border-radius: 0.3rem;
+  border: 1px solid grey;
+`;
+
+export const smallInputStyle = css`
+  width: 10rem;
+`;
+
+export const largeInputStyle = css`
+  width: 15rem;
+`;
+
+export const chatInputStyle = css`
+  width: 25rem;
+`;
