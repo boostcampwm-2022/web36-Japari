e55bf4a30cb3edef6b097f4edd5bfd0e1705bfd2
c99-coder <c99@kakao.com>
2022-11-22 17:25:41 +0900

    feat: 🎸 헤더 메인 컴포넌트 개발
    ✅ Closes: #16

---
+++ client/src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import { css } from "@emotion/react";
+
+export const headerStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 4rem;
+`;
+
+export const headerRightStyle = css`
+  display: flex;
+`;
