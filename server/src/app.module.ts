8a144c4d2d015d20cd782016a850830f88053a6f
Seungjae Lee <mathlife12345@gmail.com>
2022-11-23 14:16:49 +0900

    feat: 🎸 prisma service 구현

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 
 import { AppService } from "./app.service";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
   imports: [],
