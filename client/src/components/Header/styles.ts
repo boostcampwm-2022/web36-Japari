872ae7f463eb38b1c0ec5d9800aed51b03d339b7
c99-coder <c99@kakao.com>
2022-11-23 14:11:10 +0900

    style: 💄 헤더 컴포넌트 margin을 padding으로 변경

--- client/src/components/Header/styles.ts
+++ client/src/components/Header/styles.ts
@@ -5,7 +5,7 @@ export const headerStyle = css`
   justify-content: space-between;
   align-items: center;
   height: 4rem;
-  margin: 0.5rem 1rem 0 1rem;
+  padding: 0.5rem 2rem 0 1rem;
 `;
 
 export const headerRightStyle = css`
