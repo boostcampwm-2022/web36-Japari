b7efb4f04bddab7abf72146df9225a76eae6c89a
tnghd5761 <tnghd5761@naver.com>
2022-11-23 10:48:23 +0900

    feat: 🎸 로비페이지 라우팅 설정, 랜딩페이지 파일명 수정
    ✅ Closes: #24

---
+++ client/src/pages/lobby/index.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import * as style from "./styles";
+import { Page } from "../../components/Page";
+
+const LobbyPage: React.FC = () => {
+  return (
+    <Page>
+      <p>lobby page</p>
+    </Page>
+  );
+};
+
+export default LobbyPage;
