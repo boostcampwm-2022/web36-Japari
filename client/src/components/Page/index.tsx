29452ee3a9a740ee546342b1259e542187f908e1
c99-coder <c99@kakao.com>
2022-11-23 16:46:58 +0900

    feat: 🎸 페이지 URL에 따라 headerType 동적 변환
    ✅ Closes: #34

--- client/src/components/Page/index.tsx
+++ client/src/components/Page/index.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Header } from "../Header";
 import * as style from "./styles";
 
@@ -8,9 +9,27 @@ interface PageProps {
 }
 
 export const Page: React.FC<PageProps> = ({ children }) => {
+  const location = useLocation();
+
+  let headerType: "랜딩" | "로비" | "게임 대기실" | "게임 진행" = "랜딩";
+  switch (location.pathname.split("/")[1]) {
+    case "":
+      headerType = "랜딩";
+      break;
+    case "lobby":
+      headerType = "로비";
+      break;
+    case "waiting":
+      headerType = "게임 대기실";
+      break;
+    case "playing":
+      headerType = "게임 진행";
+      break;
+  }
+
   return (
     <div css={style.PageContainer}>
-      <Header headerType="로비" />
+      <Header headerType={headerType} />
       {children}
     </div>
   );
