dfed43a892b0cb16aa73941343b9b3d29ee61d02
tnghd5761 <tnghd5761@naver.com>
2022-11-24 16:07:46 +0900

    feat: 🎸 select 컴포넌트 수정
    ✅ Closes: #38

--- client/src/components/Select/index.tsx
+++ client/src/components/Select/index.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import * as style from "./styles";
 import { css } from "@emotion/react";
+import { ChangeEvent } from "react";
 
 export interface SelectProps {
   selectType: "게임 필터" | "게임 생성" | "인원 제한" | "공개방 설정";
@@ -9,6 +10,14 @@ export interface SelectProps {
 }
 
 const Select = ({ selectType, width, setValue }: SelectProps) => {
+  const handlePrivateSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === "private") {
+      setValue(true);
+    } else {
+      setValue(false);
+    }
+  };
+
   switch (selectType) {
     case "게임 필터":
       return (
@@ -18,11 +27,11 @@ const Select = ({ selectType, width, setValue }: SelectProps) => {
               ${style.selectBox}
               width: ${width};
             `}
-            onChange={setValue ? e => setValue(e.target.value) : () => {}}
+            onChange={e => setValue(e.target.value)}
           >
             <option>모든 게임</option>
-            <option value="catchMind">캐치 마인드</option>
-            <option value="battleShip">배틀십</option>
+            <option value={1}>캐치 마인드</option>
+            <option value={2}>배틀십</option>
           </select>
         </div>
       );
@@ -35,10 +44,11 @@ const Select = ({ selectType, width, setValue }: SelectProps) => {
               ${style.selectBox}
               width: ${width};
             `}
+            onChange={e => setValue(e.target.value)}
           >
             <option>게임을 선택해 주세요.</option>
-            <option value="catchMind">캐치 마인드</option>
-            <option value="battleShip">배틀십</option>
+            <option value={1}>캐치 마인드</option>
+            <option value={2}>배틀십</option>
           </select>
         </div>
       );
@@ -51,10 +61,11 @@ const Select = ({ selectType, width, setValue }: SelectProps) => {
               ${style.selectBox}
               width: ${width};
             `}
+            onChange={e => setValue(e.target.value)}
           >
             <option>인원을 선택해 주세요.</option>
-            <option value="4">4명</option>
-            <option value="8">8명</option>
+            <option value={4}>4명</option>
+            <option value={8}>8명</option>
           </select>
         </div>
       );
@@ -68,6 +79,7 @@ const Select = ({ selectType, width, setValue }: SelectProps) => {
               width: ${width};
             `}
             defaultValue={"public"}
+            onChange={handlePrivateSelect}
           >
             <option value="public">공개방</option>
             <option value="private">비공개방</option>
