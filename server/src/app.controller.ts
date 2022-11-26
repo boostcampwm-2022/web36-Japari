f305b0b8a47f098691dc35b310b4ce01d0cf6890
Seungjae Lee <mathlife12345@gmail.com>
2022-11-26 21:02:33 +0900

    test: 💍 redis 정상 작동 확인

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Inject } from "@nestjs/common";
+import Redis from "ioredis";
 import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, @Inject("RedisProvider") private readonly redis: Redis) {}
+
+  @Get("/test")
+  test() {
+    return this.redis.get("user-1");
+  }
 }
--- server/src/modules/game/game.module.ts
+++ server/src/modules/game/game.module.ts
@@ -4,9 +4,11 @@ import { GameController } from "./game.controller";
 
 import { GameService } from "./game.service";
 import { PrismaModule } from "src/modules/prisma/prisma.module";
+import { RedisModule } from "../redis/redis.module";
+import redisConfig from "src/config/redis.config";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RedisModule.registerAsync(redisConfig)],
   controllers: [GameController],
   providers: [GameService],
 })
