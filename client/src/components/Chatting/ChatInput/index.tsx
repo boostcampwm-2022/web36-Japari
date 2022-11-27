0964522bb84e5efbaea6f476158e61d0221ec60a
tnghd5761 <tnghd5761@naver.com>
2022-11-27 18:09:15 +0900

    feat: 🎸 (fe) 채팅 Input 전송 및 채팅창 자동 스크롤다운

--- client/src/components/Chatting/ChatInput/index.tsx
+++ client/src/components/Chatting/ChatInput/index.tsx
@@ -2,14 +2,50 @@
 import Input from "../../Input";
 import chatInputEnter from "../../../assets/icons/chat-input-enter.svg";
 import * as style from "./styles";
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
+import { Chat } from "..";
 
-const ChatInput = () => {
+interface ChatInputProps {
+  addLogs: (value: Chat) => void;
+}
+
+const ChatInput = ({ addLogs }: ChatInputProps) => {
   const [message, setMessage] = useState<string>("");
+
+  const sendMessage = () => {
+    if (message === "") {
+      alert("메세지를 입력하세요");
+      return;
+    }
+    const newLog: Chat = {
+      sender: "me",
+      message: message,
+      sendTime: new Date(),
+    };
+
+    addLogs(newLog);
+
+    // socket.emit('send_message', newLog)
+
+    setMessage("");
+  };
+
+  const pressEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      sendMessage();
+    }
+  };
+
   return (
     <div css={style.ChatInputContainerStyle}>
-      <Input type="text" placeholder="메세지 입력하기..." setValue={setMessage} />
-      <img src={chatInputEnter} alt="chatLogEnter" />
+      <Input
+        type="text"
+        placeholder="메세지 입력하기..."
+        value={message}
+        setValue={setMessage}
+        handleKeyPress={pressEnter}
+      />
+      <img src={chatInputEnter} alt="chatLogEnter" onClick={sendMessage} />
     </div>
   );
 };
