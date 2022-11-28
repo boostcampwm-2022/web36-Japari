3617e59efce58d06334d2683b385cc318072c138
Seungjae Lee <mathlife12345@gmail.com>
2022-11-28 15:00:28 +0900

    fix: 🐛 config 변경으로 인한 문제 해결

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:3000",
+      target: "http://localhost:4000",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "",
