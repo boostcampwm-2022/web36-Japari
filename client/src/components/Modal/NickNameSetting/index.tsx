5c1d3539c35b47fe8e1dce4bd27e97d35fc3ffeb
tnghd5761 <tnghd5761@naver.com>
2022-11-28 16:13:18 +0900

    feat: 🎸 닉네임이 null이면 닉네임 모달 open

--- client/src/components/Modal/NickNameSetting/index.tsx
+++ client/src/components/Modal/NickNameSetting/index.tsx
@@ -4,12 +4,27 @@ import Input from "../../Input";
 import Button from "../../Button";
 import { useState } from "react";
 
-const NickNameSetting = () => {
+interface NickNameSettingProps {
+  closeModal: () => void;
+}
+
+const NickNameSetting = ({ closeModal }: NickNameSettingProps) => {
   const [nickName, setNickName] = useState<string>("");
+
+  const handleSubmitButton = () => {
+    if (nickName === "") {
+      alert("닉네임을 입력하세요.");
+      return;
+    }
+    // 닉네임 설정 api
+
+    closeModal();
+  };
+
   return (
     <>
-      <Input type="text" placeholder="" setValue={setNickName} />
-      <Button buttonType="확인" handleClick={() => {}} />
+      <Input type="text" placeholder="" value={nickName} setValue={setNickName} />
+      <Button buttonType="확인" handleClick={handleSubmitButton} />
     </>
   );
 };
--- client/src/pages/lobby/index.tsx
+++ client/src/pages/lobby/index.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as style from "./styles";
 import { Page } from "../../components/Page";
 import Profile from "../../components/Profile";
@@ -10,15 +10,27 @@ import * as dummy from "../dummy";
 import { useRecoilState } from "recoil";
 import { userState } from "../../recoil/user";
 import { getLoggedInUser } from "../../api/user";
+import Modal from "../../components/Modal";
 
 const LobbyPage: React.FC = () => {
   const [user, setUser] = useRecoilState(userState);
+  const [nicknameModalOpen, setNicknameModalOpen] = useState<boolean>(false);
+
+  const closeModal = () => {
+    setNicknameModalOpen(false);
+  };
 
   useEffect(() => {
     getLoggedInUser().then(res => {
       setUser(res);
     });
   }, []);
+
+  useEffect(() => {
+    if (!user) return;
+
+    if (!user.nickname) setNicknameModalOpen(true);
+  }, [user]);
 
   return (
     <Page>
@@ -32,6 +44,7 @@ const LobbyPage: React.FC = () => {
           <Chatting />
         </div>
       </div>
+      {nicknameModalOpen && <Modal ModalType="닉네임 설정" closeModal={closeModal} />}
     </Page>
   );
 };
