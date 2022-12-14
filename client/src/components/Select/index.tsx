d74795385dab8366b50e0bc0b74aef22a2763267
Seungjae Lee <mathlife12345@gmail.com>
2022-12-14 13:56:44 +0900

    fix: 🐛 select에서 battleship을 선택할 수 없도록 수정
    ✅ Closes: #147

--- client/src/components/Select/index.tsx
+++ client/src/components/Select/index.tsx
@@ -32,7 +32,6 @@ const Select = ({ selectType, width, setValue }: SelectProps) => {
           >
             <option value={0}>모든 게임</option>
             <option value={1}>캐치 마인드</option>
-            <option value={2}>배틀십</option>
           </select>
         </div>
       );
@@ -52,7 +51,6 @@ const Select = ({ selectType, width, setValue }: SelectProps) => {
               게임을 선택해 주세요.
             </option>
             <option value={1}>캐치 마인드</option>
-            <option value={2}>배틀십</option>
           </select>
         </div>
       );
